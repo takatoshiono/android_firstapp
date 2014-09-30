2f8766f1545383de7ef178f3ea650b70a99bbd24
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-09-30 19:48:00 +0900

    Build an Intent

--- app/src/main/java/com/example/usr0600268/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/usr0600268/myfirstapp/MyActivity.java
@@ -1,12 +1,17 @@
 package com.example.usr0600268.myfirstapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends Activity {
+
+    private static final String EXTRA_MESSAGE = "com.example.usr0600268.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +37,14 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
